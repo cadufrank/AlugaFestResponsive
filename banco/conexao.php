f27dcb626cd11d5f0a03389c250614cbefe54fb5
cadufrank <carlosfrank.dev@gmail.com>
2023-06-01 12:35:32 -0300

    Upload de nova branch para conexão do banco de dados

---
+++ banco/conexao.php
@@ -0,0 +1,18 @@
+<?php
+
+    $dbHost  ='Localhost';
+    $dbUsername ='root';
+    $dbPassword ='';
+    $dbname = 'alugafest';
+
+    // $conexao = new mysqli( $dbHost,$dbUsername,$dbPassword,$dbname);
+    
+    //  if ($conexao ->connect_errno)
+    // {
+    //    echo "Erro";
+    // }
+    //   else 
+    //  {
+    //       echo "Conexao efetuada com Sucesso";
+    //  }
+?>
